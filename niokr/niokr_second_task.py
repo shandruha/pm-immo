0f485f6bc5c2c87fd1d78141b3e53b73e4994f68
shandruha <50118328+shandruha@users.noreply.github.com>
2019-05-01 09:11:06 +0300

    Исправил phi на psi

--- niokr/niokr_second_task.py
+++ niokr/niokr_second_task.py
@@ -1,3 +1,9 @@
+"""Алгоритм построен на основе вкладок из главы 10.2 (стр. 223) учебника.
+Обратите внимание, что пример в книге не содержит членов первой степени в выражении для функций f_1 и f_2.
+По идее, производная psi(t) в таком случае имеет разрыв первого рода, что означает отсутствие обратной функции, а значит u(t) так просто выразить нельзя.
+Одним словом, я доверился Людковскому...
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
@@ -27,7 +33,7 @@ def second_task_opt(a_1, b_1, c_1, d_1, a_2, b_2, c_2, d_2, x_0, T, n_points, r_
         eps (float): эпсилон - уровень погрешности (тоже из условия)
     Returns:
         tuple(numpy.array, numpy.array, numpy.array, numpy.array): 
-            вектор времени t, вектор процесса x, вектор управления u, вектор функции фи phi
+            вектор времени t, вектор процесса x, вектор управления u, вектор функции psi
     """
     # Вектор времени
     t = np.linspace(0, T, n_points)
@@ -38,8 +44,8 @@ def second_task_opt(a_1, b_1, c_1, d_1, a_2, b_2, c_2, d_2, x_0, T, n_points, r_
     r = r_func(t)
     # Вектор самого процесса x(t)
     x = np.zeros(t.shape)
-    # Функция фи
-    phi = np.zeros(t.shape)
+    # Функция psi(t)
+    psi = np.zeros(t.shape)
     # Вектор со значениями функции управления u(t)
     u = np.zeros(t.shape)
 
@@ -51,24 +57,24 @@ def second_task_opt(a_1, b_1, c_1, d_1, a_2, b_2, c_2, d_2, x_0, T, n_points, r_
         x[-1] = s
         
         if x[-1] >= r[-1]:
-            phi[-1] = -2 * a_1 * (x[-1] - r[-1]) - c_1
+            psi[-1] = -2 * a_1 * (x[-1] - r[-1]) - c_1
         else:
-            phi[-1] = -2 * b_1 * (x[-1] - r[-1]) - d_1
+            psi[-1] = -2 * b_1 * (x[-1] - r[-1]) - d_1
 
         for i in range(2, t.shape[0]+1):
-            u_ = (phi[-i+1] - c_2) / 2 / a_2
+            u_ = (psi[-i+1] - c_2) / 2 / a_2
 
             if u_ >= 0:
                 u[-i] = u_
             else:
-                u[-i] = (phi[-i+1] - d_2) / 2 / b_2
+                u[-i] = (psi[-i+1] - d_2) / 2 / b_2
 
             x[-i] = x[-i+1] - dt * u[-i]
             
             if x[-i] >= r[-i]:
-                phi[-i] = phi[-i+1] - dt * (2 * a_1 * (x[-i] - r[-i]) + c_1)
+                psi[-i] = psi[-i+1] - dt * (2 * a_1 * (x[-i] - r[-i]) + c_1)
             else:
-                phi[-i] = phi[-i+1] - dt * (2 * b_1 * (x[-i] - r[-i]) + d_1)
+                psi[-i] = psi[-i+1] - dt * (2 * b_1 * (x[-i] - r[-i]) + d_1)
 
         delta = abs(x[0] - x_0)
 
@@ -77,12 +83,12 @@ def second_task_opt(a_1, b_1, c_1, d_1, a_2, b_2, c_2, d_2, x_0, T, n_points, r_
     # Находим минимум функции с заданным уровнем погрешности
     minimize(f, method='CG', x0=r[-1], tol=eps)
         
-    return t, x, u, phi
+    return t, x, u, psi
 
 
 if __name__ == '__main__':
     # Вариант 1
-    t, x, u, phi = second_task_opt(a_1=1/2,
+    t, x, u, psi = second_task_opt(a_1=1/2,
                                    b_1=1,
                                    c_1=1/3,
                                    d_1=7/5,
@@ -103,9 +109,9 @@ if __name__ == '__main__':
     ax[0].set_xlabel('t')
     ax[0].set_ylabel('x(t)')
 
-    ax[1].plot(t, phi)
+    ax[1].plot(t, psi)
     ax[1].set_xlabel('t')
-    ax[1].set_ylabel('phi(t)')  
+    ax[1].set_ylabel('psi(t)')  
 
     ax[2].plot(t, u)
     ax[2].set_xlabel('t')
@@ -114,7 +120,7 @@ if __name__ == '__main__':
     plt.show()
 
     # Результаты в виде таблицы
-    df = pd.DataFrame({'t': t, 'x': x, 'u': u, 'phi': phi}) \
+    df = pd.DataFrame({'t': t, 'x': x, 'u': u, 'psi': psi}) \
             .set_index('t')
 
     # Сохраняем в книгу Excel
